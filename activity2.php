458216c0104a2f6b8b835255d49b7ac97cf3d96e
Noel Pactanac <202280151@psu.palawan.edu.ph>
2025-03-11 12:31:57 +0800

    fix date sticking to one year

--- activity2.php
+++ activity2.php
@@ -47,7 +47,7 @@ $faker = Faker\Factory::create();
                     echo "<td>" . $faker->words(1, true) . "</td>";
                     echo "<td>" . $faker->name . "</td>";
                     echo "<td>" . $faker->randomElement($genres) . "</td>";
-                    echo "<td>" . $faker->year($min = 1900, $max = 2024) . "</td>";
+                    echo "<td>" . $faker->year('1900,2024') . "</td>";
                     echo "<td>" . $faker->isbn13 . "</td>";
                     echo "<td>" . $faker->paragraph . "</td>";
                     echo "</tr>";
